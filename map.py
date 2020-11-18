7a259e1447a2b65a3ea058eed46a2805a3e7bff4
dis·therapy <56242810+distherapy@users.noreply.github.com>
2020-11-18 10:35:55 -0500

    Add files via upload

---
+++ map.py
@@ -0,0 +1,69 @@
+#!usr/bin/env python
+#
+#  <sixie6e@paracosmoclast>
+#
+import numpy as np
+import pandas as pd
+
+class food():
+	kelloggs = [cheez-it, eggo, famous amos, keebler, town house] #2017 revenue: $12.93 billion USD
+
+
+	general mills = [betty crocker, bisquick, gold medal, cheerios, chex] #2017 revenue: $15.62 billion USD
+
+
+	kraft-heinz = [heinz ketchup, kraft mac & cheese, lunchables, maxwell] #2017 revenue: $18.22 billion
+
+
+	mondelez = [cadbury, chips ahoy!, nabisco, oreo] #2017 revenue: $25.9 billion
+
+
+	mars = [m&ms, snickers, dove, uncle ben’s] #2017 revenue: $35 billion
+
+
+	coca-cola = [coca-cola, minute maid, glaceau] #2017 revenue: $35.41 billion
+
+	unilever = [ben & jerry’s, klondike, popsicle, degree, vaseline] #2017 revenue: $62.62 billion
+
+
+	procter&gamble = [pampers, tide, downy, charmin, gillette, crest] #2017 revenue: $65.06 billion
+
+
+	pepsico = [pepsi, frito-lay, quaker, tropicana] #2017 revenue: $65.53 billion
+
+
+	johnson&johnson = [aveeno, clean & clear, band-aid, tylenol] #2017 revenue: $76.45 billion
+
+
+	nestle = [toll house, gerber, poland spring, stouffer’s] #2017 revenue: $89.79 billion
+
+class vehicle():
+	volkswagen group = [audi, bentley, bugatti, lamborghini, porsche, seat, skoda, volkswagen] #2017 revenue $272.21 billion
+
+	toyota = [toyota, daihatsu, lexus] #2017 revenue $256.65 billion
+	
+	ford motor company = [ford, lincoln, troller] #2017 revenue $41.3 billion
+	
+	general motors = [cadillac, gmc, chevrolet, holden] #2017 revenue $145.58 billion
+	
+	renault-nissan-mitsubishi alliance = [renault, nissan, infiniti, dacia, datsun, samsung renault, lada, mitsubishi] #2017 revenue: $181.64 billion
+	
+	hyundai motor group = [hyundai, kia] #2017 revenue: $90.87 billion
+	
+	daimler ag = [mercedes-benz, smart, amg] #2017 revenue: $195.01 billion
+	
+	fiat chrysler automobiles = [alfa romeo, dodge, lancia, maserati, chrysler, fiat, jeep, ram] #2017 revenue: $131.62 billion
+	
+	honda = [honda, acura] #2017 revenue: $134.68 billion
+	
+	bmw = [bmw, mini, rolls royce] #2017 revenue: $117.12 billion
+	
+	suzuki = [suzuki, maruti suzuki] #2017 revenue: $30.52 billion
+	
+	groupe psa = [peugeot, ds automobiles, opel, citroen, vauxhall] #2017 revenue: $77.38 billion
+	
+	tata motors = [tata, jaguar, land rover] #2017 revenue: $41 billion
+	
+	geely = [lotus, proton, volvo] #2017 revenue: $14.13 billion
+	
+	independent = [ferrari $4.05B, aston martin $1.16B, subaru $32.02B, mazda $30.9B, tesla $11.76B, mclaren $1.15B]
