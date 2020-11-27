0cb38e9bbc8bc86adfd14c3da4fab414568236bd
dis·therapy <56242810+distherapy@users.noreply.github.com>
2020-11-26 19:17:27 -0500

    Create subcat.py

---
+++ subcat.py
@@ -0,0 +1,38 @@
+#!usr/bin/env python
+#
+#<sixie6e@paracosmoclast>
+#
+#pseudolayout
+#
+####################################
+'''
+industry:
+	corp(s):
+		business:
+				brand:
+					product
+
+retail:
+	gen_mills:
+		food:
+			haagen_dazs:
+				banana pb chip
+
+media:
+	natl_amusements:
+		film:
+			paramount:
+				interstellar
+
+vehicle:
+	toyota:
+		car:
+			lexus:
+				gx400
+
+retail:
+	cke:
+		fast_food:
+				carls_jr:
+					famous_star
+'''
